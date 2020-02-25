c3e6e0199e17a90acea31ccc5202c9daf3f5588c
abrikos <abrikos1100@gmail.com>
2020-02-25 13:36:23 +0300

    Modified step sequence in shellsort. Added some data files

--- sorts-homework/shellsort.c
+++ sorts-homework/shellsort.c
@@ -20,13 +20,13 @@ int step(int n, int i) {
     int j, ans = 1;
     // from "A new upper bound for shellsort. Robert Sedgewick"
     // https://doi.org/10.1016/0196-6774(86)90001-5
-    for (j = 0; ans < n; ++j)
-        ans = 1 + 9 * (1 << (2*j)) - 9 * (1 << j); // 1 + 9 * (4**j - 2**j)
+    for (j = 1; ans < n; ++j)
+        ans = 2 * (j == 1) + (1 << j - 3) * (1 << (j+1) - 3); // abs(2**j-3) * (2**(j+1)-3)
     j -= 1+i;
     if (j < 0)
         return 0;
 
-    return 1 + 9 * (1 << (2*j)) - 9 * (1 << j); // 1 + 9 * (4**j - 2**j)
+    return 2 * (j == 1) + (1 << j - 3) * (1 << (j+1) - 3); // abs(2**j-3) * (2**(j+1)-3)
 }
 
 void shellsort(
